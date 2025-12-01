4d2a51e16cd3c963daf970391e1d05e7e83b0130
xhaidendsouza <xhaidendsouza@gmail.com>
2025-12-01 09:52:25 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -26,17 +26,3 @@ for line in input:
         count += abs(num//100) + (1 if (num%100 == 0) else 0) - (1 if (num == -int(line[1:])) else 0)
         num %= 100
 print(count)
-
-x = 50
-cnt = 0
-for line in input:
-    s = line.strip()
-    k = int(s[1:])
-    if s[0] == 'L':
-        x -= k
-    else:
-        x += k
-    cnt += abs(x//100)
-    x %= 100
-print(cnt)
-# NOT 6580
